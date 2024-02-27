3a38fe3d41d9acb05fc39ed4605f34b63a8b3c38
Sadia Abass <104012271+Sadia-Abass@users.noreply.github.com>
2024-02-27 16:54:03 +0000

    Added products controller

---
+++ Luxe/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using Luxe.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Luxe.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public ProductController(IProductRepository productRepository, ICategoryRepository categoryRepository)
+        {
+            _productRepository = productRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public IActionResult List()
+        {
+            return View(_productRepository.AllProducts);
+        }
+    }
+}
