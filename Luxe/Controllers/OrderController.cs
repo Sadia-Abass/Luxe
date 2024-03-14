210f6f5c7de7df00f58e3242189424c4da68d80a
Sadia Abass <104012271+Sadia-Abass@users.noreply.github.com>
2024-03-14 18:34:38 +0000

    Add validation to checkout page

--- Luxe/Controllers/OrderController.cs
+++ Luxe/Controllers/OrderController.cs
@@ -19,5 +19,32 @@ namespace Luxe.Controllers
         {
             return View();  
         }
+
+        [HttpPost]
+        public IActionResult Checkout(Order order)
+        {
+            var item = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = item;
+
+            if(_shoppingCart.ShoppingCartItems.Count == 0)
+            {
+                ModelState.AddModelError("", "Your cart is empty, add an item first");
+            }
+
+            if(ModelState.IsValid)
+            {
+                _orderRepository.CreateOrder(order);
+                _shoppingCart.ClearCart();
+                return RedirectToAction("CheckoutComplete");
+            }
+
+            return View(order);
+        }
+
+        public IActionResult CheckoutComplete()
+        {
+            ViewBag.CheckoutCompleteMessage = "Thanks for your oder. You'll soon enjoy our beauty product!";
+            return View();
+        }
     }
 }
