8a414b6c2e5180b8f9d6d3c98a6d395bd826747c
Sadia Abass <104012271+Sadia-Abass@users.noreply.github.com>
2024-03-19 11:48:16 +0000

    Add unit test for product controller

---
+++ LuxeTests/Controllers/ProductControllerTests.cs
@@ -0,0 +1,34 @@
+﻿using Luxe.Controllers;
+using Luxe.ViewModels;
+using LuxeTests.Mocks;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LuxeTests.Controllers
+{
+    public class ProductControllerTests
+    {
+        [Fact]
+        public void List_EmptyCategory_ReturnsAllProducts()
+        {
+            // Arrange
+            var mockCategoryRepository = RepositoryMocks.GetCategoryRepository();
+            var mockProductRepository = RepositoryMocks.GetProductRepository();
+
+            var productController = new ProductController(mockProductRepository.Object, mockCategoryRepository.Object);
+
+            // Act
+            var result = productController.List("");
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var productListViewModel = Assert.IsAssignableFrom<ProductListViewModel>(viewResult.ViewData.Model);
+            Assert.Equal(3, productListViewModel.Products.Count());
+        }
+    }
+}
